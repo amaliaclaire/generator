734763800767387133f519a53095befcc9e4fe26
Amalia <amalia.fergin@gmail.com>
2019-05-29 13:39:16 -0700

    seeds & migrations are seeded

--- db/seeds/1_responses.js
+++ db/seeds/1_responses.js
@@ -4,9 +4,54 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('responses').insert([
-        {id: 1, quote: 'test', category: 'testcategory', rank: 'testrank'},
-        {id: 2, quote: 'test', category: 'testcategory2', rank: 'testrank2'},
-        {id: 3, quote: 'test', category: 'testcategory3', rank: 'testrank3'},
+        {
+          id: 1,
+          quote: 'You should be neutered',
+          category: 'fuckboy',
+          rank: 1
+        },
+        {
+          id: 2,
+          quote: 'You won the worst fuck boy award. Congrats.',
+          category: 'fuckboy',
+          rank: 'testrank2'
+        },
+        {
+          id: 3,
+          quote: "you're a joke.",
+          category: 'fuckboy',
+          rank: 1
+        },
+        {
+          id: 4,
+          quote: "does your mother know this is how you talk to women",
+          category: 'concerned citizen',
+          rank: 2
+        },
+        {
+          id: 5,
+          quote: "I accidently swiped right, don't get too excited",
+          category: 'concerned citizen',
+          rank: 2
+        },
+        {
+          id: 6,
+          quote: "http://wwww.improveyoursocialskills.com/conversations",
+          category: 'concerned citizen',
+          rank: 2
+        },
+        {
+          id: 7,
+          quote: "you realize you just cock blocked yourself right?",
+          category: 'destroy manhood',
+          rank: 3
+        },
+        {
+          id: 8,
+          quote: "darwinsim, in it's full form",
+          category: 'destroy manhood',
+          rank: 3
+        },
       ]);
     });
 };
